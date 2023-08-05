79c9f092e83ca8415ba09151ddfc6869756375fb
Natalie Benjes <natalie.benjes@gmail.com>
2023-08-05 14:00:50 -0700

    add splash page info to home index

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,15 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Linq;
 
 namespace Factory.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
     {
-
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        return View();
-      }
-
+      _db = db;
     }
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      var engineers = _db.Engineers.ToList();
+      var machines = _db.Machines.ToList();
+
+      ViewBag.Engineers = engineers;
+      ViewBag.Machines = machines;
+
+      return View();
+    }
+
+  }
 }
