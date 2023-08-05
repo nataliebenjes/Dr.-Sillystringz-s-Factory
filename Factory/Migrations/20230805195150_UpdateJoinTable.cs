9b6abee0c86b411b065303fd5e6aff8bfb435bf7
Natalie Benjes <natalie.benjes@gmail.com>
2023-08-05 12:59:02 -0700

    add SQL dump file

---
+++ Factory/Migrations/20230805195150_UpdateJoinTable.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Factory.Migrations
+{
+    public partial class UpdateJoinTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Machines",
+                keyColumn: "MachineName",
+                keyValue: null,
+                column: "MachineName",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MachineName",
+                table: "Machines",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Engineers",
+                keyColumn: "PersonName",
+                keyValue: null,
+                column: "PersonName",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PersonName",
+                table: "Engineers",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MachineName",
+                table: "Machines",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PersonName",
+                table: "Engineers",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
---
+++ factory.sql
@@ -0,0 +1,67 @@
+-- MySQL dump 10.13  Distrib 8.0.15, for macos10.14 (x86_64)
+--
+-- Host: localhost    Database: factory
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `__EFMigrationsHistory`
+--
+
+LOCK TABLES `__EFMigrationsHistory` WRITE;
+/*!40000 ALTER TABLE `__EFMigrationsHistory` DISABLE KEYS */;
+INSERT INTO `__EFMigrationsHistory` VALUES ('20230804215619_Initial','6.0.0'),('20230805003641_UpdateFieldName','6.0.0'),('20230805195150_UpdateJoinTable','6.0.0');
+/*!40000 ALTER TABLE `__EFMigrationsHistory` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `EngineerMachines`
+--
+
+LOCK TABLES `EngineerMachines` WRITE;
+/*!40000 ALTER TABLE `EngineerMachines` DISABLE KEYS */;
+INSERT INTO `EngineerMachines` VALUES (1,1,1),(2,2,1),(3,2,2);
+/*!40000 ALTER TABLE `EngineerMachines` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `Engineers`
+--
+
+LOCK TABLES `Engineers` WRITE;
+/*!40000 ALTER TABLE `Engineers` DISABLE KEYS */;
+INSERT INTO `Engineers` VALUES (1,'Sarah Stewart the 3rd'),(2,'Hannah Krivickas');
+/*!40000 ALTER TABLE `Engineers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `Machines`
+--
+
+LOCK TABLES `Machines` WRITE;
+/*!40000 ALTER TABLE `Machines` DISABLE KEYS */;
+INSERT INTO `Machines` VALUES (1,'comput0r'),(2,'Lilly Pad Float machine');
+/*!40000 ALTER TABLE `Machines` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-08-05 12:58:00
